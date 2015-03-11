2e071d6ffc672ce5eecfb46abd8607f21e19f0cd
ccl2of4 <drconnoramerica@utexas.edu>
2015-03-10 21:20:12 -0500

    removed public access modifier from class definition

--- src/main/java/me/ccl2of4/CraftingAssistance/JavaPluginCommandHandler/CAAboutCommandHandler.java
+++ src/main/java/me/ccl2of4/CraftingAssistance/JavaPluginCommandHandler/CAAboutCommandHandler.java
@@ -6,7 +6,7 @@ import org.bukkit.command.CommandSender;
 /**
  * Created by connor on 3/9/15.
  */
-public class CAAboutCommandHandler extends JavaPluginCommandHandler {
+class CAAboutCommandHandler extends JavaPluginCommandHandler {
     public void execute (CommandSender sender, Command cmd, String s, String[] args) {
         sender.sendMessage ("Made by ccl2of4 - https://github.com/ccl2of4/CraftingAssistance");
     }
